33c19f55031439d24d75a00f2fb6fef259a7fa62
kezie14 <kezie.stewart@outlook.com>
2022-12-03 13:25:08 -0700

    updated local storage

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+$ (document).ready(function (){
+    console.log("hi")
+    $(".saveBtn").on("click", function(){
+        var value = $(this).siblings(".description").val()
+        var time = $(this).parent().attr("id")
+
+
+        localStorage.setItem(time, value)
+
+    })
+    $("#9 .description").val(localStorage.getItem("9"))
+    $("#10 .description").val(localStorage.getItem("10"))
+    $("#11 .description").val(localStorage.getItem("11"))
+    $("#12 .description").val(localStorage.getItem("12"))
+    $("#13 .description").val(localStorage.getItem("13"))
+    $("#14 .description").val(localStorage.getItem("14"))
+    $("#15 .description").val(localStorage.getItem("15"))
+    $("#16 .description").val(localStorage.getItem("16"))
+    $("#17 .description").val(localStorage.getItem("17"))
+  
+
+    $ ("#currentDay").text(moment().format("dddd, MMMM Do"))
+})
+
+
